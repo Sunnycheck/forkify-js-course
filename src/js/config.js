53a28f86198e549b857c4e56ef1b3ff1debef0cf
Sunnycheck <ppalex.g@gmail.com>
2022-12-05 00:55:47 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 15; /* initial value = 10!!!*/
+export const RES_PER_PAGE = 10;
+export const KEY = '6711b79c-a5e2-4b28-a12e-8d0d5b69e9c9';
+export const MODAL_CLOSE_SEC = 2.5;
